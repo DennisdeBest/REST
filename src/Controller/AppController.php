999cb3e8a70f30f3d679ed5d8551472b00e450e2
DennisdeBest <dennis@debest.fr>
2018-01-18 12:54:29 +0100

    FOSUSER, FOSREST, JWT

---
+++ src/Controller/AppController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dennisdebest
+ * Date: 18/01/18
+ * Time: 11:16
+ */
+
+class AppController
+{
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dennisdebest
+ * Date: 18/01/18
+ * Time: 12:14
+ */
+
+class User
+{
+
+}
